5d841858f800464fbe37b97fb807531e454d2b4d
Glaiton Lemos dos Santos <glaiton.santos@jmt.com.br>
2020-11-24 16:46:36 -0300

    adicionando banco h2 para teste

--- src/main/java/com/glaitonsantos/webservice/domain/UserDomain.java
+++ src/main/java/com/glaitonsantos/webservice/domain/UserDomain.java
@@ -1,10 +1,17 @@
 package com.glaitonsantos.webservice.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class UserDomain implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
